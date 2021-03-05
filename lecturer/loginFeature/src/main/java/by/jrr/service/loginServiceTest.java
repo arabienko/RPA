bd16159072703f7fcc690c9a211413434ca7c2bd
Tatsiana Arabiyenka <arabienko@tut.by>
2021-03-05 19:26:33 +0300

    adds check user password functionality

--- lecturer/loginFeature/src/main/java/by/jrr/service/loginServiceTest.java
+++ lecturer/loginFeature/src/main/java/by/jrr/service/loginServiceTest.java
@@ -1,7 +1,45 @@
 package by.jrr.service;
 
-import static org.junit.Assert.*;
+import by.jrr.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class loginServiceTest {
 
+    loginService loginService;
+
+    @Before
+    public void setup() {
+        this.loginService = new loginService();
+    }
+
+    @Test
+    public void checkUserPassword_positiv() {
+        User user = getUser();
+
+        String userInput = "password";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void checkUserPassword_negativ() {
+        User user = getUser();
+
+        String userInput = "wrong";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 }
+
+
+
